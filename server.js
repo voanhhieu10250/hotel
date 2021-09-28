0a2d70918730324caf541fbd3173fbcceda14e08
Vo Anh Hieu <voanhhieu10250@gmail.com>
2021-09-29 00:55:29 +0700

    setup gulp

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 8080;
+const buildPath =
+  process.env.NODE_ENV === 'production' ? '/build' : '/packages/hotel/build';
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'packages/hotel/build')));
+app.use(express.static(path.join(__dirname, buildPath)));
 
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
